94e281729d7153b0f4c76705cfb3acec62ac96b2
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-07-18 14:00:05 +0530

    Add files via upload

---
+++ type_conve2.java
@@ -0,0 +1,15 @@
+public class type_conve2 {
+    public static void main(String args[])
+    {
+        char c='a';
+        char b='A';
+        char d='z';
+        int num=c;
+        int num1=b;
+        int num2=d;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+    
+}
---
+++ type_conversion.java
@@ -0,0 +1,11 @@
+public class type_conversion {
+    public static void main(String args[]){
+        // right
+        int a=10;                   
+        long b=a;
+        // wrong
+    //     long a=10;
+    //    int b=a;
+        System.out.println(b);
+    }
+}
