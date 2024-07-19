693614949bd55d0b470358e50d8e0e3340676e0d
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-07-19 16:49:01 +0530

    Add files via upload

---
+++ add5.java
@@ -0,0 +1,10 @@
+public class add5{
+    public static void main(String args[]){
+        byte a='a';
+        int b='b';
+        char c='c';
+        byte ans=(byte)(a+b+c);
+        // char ans=a+b+c;
+        System.out.println(ans);
+    }
+}
