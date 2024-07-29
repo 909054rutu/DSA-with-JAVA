6e6f9ff15ca24c83b9432228aa8cc7e0b1f83e75
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-07-29 23:38:55 +0530

    Function

---
+++ fun_product.java
@@ -0,0 +1,22 @@
+public class fun_product {
+    public static int product(int a,int b){
+        int prod=a*b;
+    
+      return prod;
+    }
+
+public static void main(String args[]){
+    System.out.println(product(3,4));
+}
+}
+/*public class fun_product {
+    public static void product(int a,int b){
+        int prod=a*b;
+    System.out.println(prod);
+      
+    }
+
+public static void main(String args[]){
+    product(4,5);
+}
+}*/
