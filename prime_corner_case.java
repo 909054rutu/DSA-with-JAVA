83a15af259b58a1290016a23fdbf24df1e619f91
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-07-30 16:36:10 +0530

    prime function

---
+++ prime_corner_case.java
@@ -0,0 +1,22 @@
+public class prime_corner_case {
+         
+        public static boolean isPrime(int n){
+            //corner case
+            if(n==2){
+                return true;
+            }
+            boolean isPrime=true;
+            for(int i=2;i<=n-1;i++){
+                if(n%i==0){
+                isPrime=false;
+                break;
+                }
+            }
+        return isPrime;
+        }
+        public static void main(String args[]){
+            System.out.println(isPrime(12));
+        }
+        }
+    
+
