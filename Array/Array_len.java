4d2a5609d0659e95399c2029b004aa2f55d13c4c
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-08-04 11:05:18 +0530

    Add files via upload

---
+++ Array/Array_len.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Array_len{
+    public static void main(String args[]){
+        int a[]=new int[100];
+        System.out.println("Length Of Array:"+a.length);
+
+    }
+    
+}
