4680618852c44ca403182a308a6da8e0160b0674
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-08-31 14:43:38 +0530

    Add files via upload

---
+++ Advance Pattern/python_pattern.java
@@ -0,0 +1,20 @@
+public class python_pattern{
+    public static void pattern(int n){
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j=i-1;j>0;j--){
+                System.out.print(j);
+            }
+            System.out.println();        
+        }
+    }
+    public static void main(String args[]){
+        pattern(4);
+    }
+}
