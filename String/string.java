a2c094910752f0ecf57a458573373fe3dde8be0c
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-09-28 21:28:17 +0530

    Add files via upload

---
+++ String/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String args[]){
+        // char s[]={'A','B','C','D','E'};
+        String st="ABCD";
+        String str=new String("xyz");
+        System.out.println("String="+st);
+        System.out.println("String="+str);
+    }
+}
