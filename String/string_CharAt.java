85ec5fa3db7460d908fa3e67f960f00c4113a198
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-09-28 21:28:52 +0530

    Add files via upload

---
+++ String/string_CharAt.java
@@ -0,0 +1,8 @@
+public class string_CharAt {
+       public static void main(String args[]){
+        String str="Swami";
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+       }
+}
